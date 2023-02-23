f6b7f529cc9770c2ba632c34c1b15f00bc82bc07
Shahrin Reza <91687626+shahrinreza21@users.noreply.github.com>
2023-02-23 18:32:45 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const counter = window.counter.default
+
+const callback = entries => {
+	entries.forEach( entry => {
+		const el = entry.target
+		if ( entry.isIntersecting && ! el.classList.contains( 'is-visible' ) ) {
+			counter( el, {
+				duration: 2000,
+				delay: 16,
+			} )
+			el.classList.add( 'is-visible' )
+		}
+	} )
+}
+
+const IO = new IntersectionObserver( callback, { threshold: 1 } )
+
+const el = document.querySelector( '.counter' )
+IO.observe( el )
